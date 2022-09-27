355199f3a15a2623bf3eb414f069c72d977d6bdd
hoshico <melancholinista.yasu@gmail.com>
2022-09-27 16:53:50 +0900

    add cweInput

--- src/pages/inputList/index.tsx
+++ src/pages/inputList/index.tsx
@@ -18,6 +18,17 @@ const InputListPage = () => {
     const removeCveList = [...cveList]
     setCveList(removeCveList)
   }
+
+  const onClickCweAdd = () => {
+    const newCweList = [...cweList, cwe]
+    setCweList(newCweList)
+    setCwe('')
+  }
+  const onClickCweRemove = (index: number) => {
+    cweList.splice(index, 1)
+    const removeCweList = [...cweList]
+    setCveList(removeCweList)
+  }
   return (
     <>
       <Box m={2}>
@@ -31,10 +42,40 @@ const InputListPage = () => {
         </Box>
         {cveList.length > 0 &&
           cveList.map((cve, index) => (
-            <Box mt={2} key={index} sx={{ display: 'flex', alignItems: 'center' }}>
-              <Typography>・{cve}</Typography>
-              <Box ml={2}>
-                <Button variant='outlined' onClick={() => onClickRemove(index)}>削除</Button>
+            // 1つのcve
+            <Box key={index}>
+              <Box mt={2} sx={{ display: 'flex', alignItems: 'center' }}>
+                <Typography>・{cve}</Typography>
+                <Box ml={2}>
+                  <Button variant="outlined" onClick={() => onClickRemove(index)}>
+                    削除
+                  </Button>
+                </Box>
+              </Box>
+              {/*cweゾーン*/}
+              <Box m={2}>
+                <Box mt={2} sx={{ display: 'flex', alignItems: 'center' }}>
+                  <TextField variant="outlined" label="cwe番号" value={cwe} onChange={(e) => setCwe(e.target.value)} />
+                  <Box ml={2}>
+                    <Button variant="outlined" onClick={onClickCweAdd}>
+                      追加
+                    </Button>
+                  </Box>
+                </Box>
+                {cweList.length > 0 &&
+                  cweList.map((cwe, index) => (
+                    // 1つのcve
+                    <Box key={index}>
+                      <Box mt={2} sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Typography>・{cwe}</Typography>
+                        <Box ml={2}>
+                          <Button variant="outlined" onClick={() => onClickCweRemove(index)}>
+                            削除
+                          </Button>
+                        </Box>
+                      </Box>
+                    </Box>
+                  ))}
               </Box>
             </Box>
           ))}
