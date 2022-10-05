d48b4c276607838b01c783caaf641416a1c3c071
hoshico <melancholinista.yasu@gmail.com>
2022-10-05 21:43:55 +0900

    fix usefindChanges

--- src/components/hooks/useFindChages.ts
+++ src/components/hooks/useFindChages.ts
@@ -1,8 +1,5 @@
-import { useState } from "react";
-
 export const useFindChanges = (contentInfo: any, ...topics: Array<string | number>) => {
-  const [diff, setDiff] = useState(false);
   const diffArray = topics.filter((topic, index) => topic !== Object.values(contentInfo)[index])
-  diffArray.length ? setDiff(true) : setDiff(false);
+  const diff = diffArray.length ? true : false;
   return {diff, diffArray}
 };
