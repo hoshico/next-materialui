daa76293824a8a9c4f375a17c63a8041f1c5c6bb
hoshico <melancholinista.yasu@gmail.com>
2022-11-09 08:06:53 +0900

    add score-form

--- src/pages/react-form-number/index.tsx
+++ src/pages/react-form-number/index.tsx
@@ -1,5 +1,4 @@
-import { ErrorSharp, InputSharp, ResetTv, Score, SettingsOverscanOutlined } from '@mui/icons-material'
-import { Box, Button, Checkbox, FormControlLabel, Grid, Stack, TextField } from '@mui/material'
+import { Box, Button, Grid, Stack, TextField, Typography } from '@mui/material'
 import { Controller, useForm } from 'react-hook-form'
 
 type Inputs = {
@@ -37,97 +36,27 @@ const reactFormNumber = () => {
     validationRulesで複数用意
   */
   const validationRules = {
-    title: {
-      required: 'タイトルを入力してください',
-      minLength: { value: 3, message: '3文字以上で入力してください。' }
-    },
-    description: {
-      required: '概要を入力してください',
-      minLength: { value: 3, message: '3文字以上で入力してください。' }
-    },
-    important: {},
     score: {
-      min: 0,
-      max: 10,
-      maxLength: { value: 3, message: '4文字以上で入力してください。' }
+      max: { value: 10, message: "10以下で入力"},
+      min: { value: 0, message: '0以上で入力' },
+      minLength: { value: 2, message: '少数第一位までで入力' },
+      maxLength: { value: 3, message: '少数第一位までで入力' },
     },
     score2: {
       min: 0,
       max: 10,
       maxLength: { value: 3, message: '3文字以下で入力してください。' }
-    },
-    date: {}
+    }
   }
   // dataでアクセスできる
   const onSubmit = (data: Inputs) => {
-    console.log(data);
-    const key = Object.keys(getValues()) as InputsKeys
-    //const arr = getValues();
-    //type Arry = keyof typeof arr;
-    //const key = Object.keys(getValues())
-    //unregister(["title", "important", "score", "score2"], {keepDirty: false});
-    unregister(key, { keepDirty: false })
-  }
-  const onClcikGet = () => console.log(getValues())
-  const onCheckDirty = () => {
-    // dirtyFieldsでコントロール下の変更要素がわかる
-    console.log(dirtyFields)
-
-    // isDirtyでコントロール下の何かが変更
-    console.log(isDirty)
+    console.log(data)
   }
 
   return (
     <Stack m={4} component="form" noValidate onSubmit={handleSubmit(onSubmit)}>
       {/*ControllerコンポーネントでReactHookFormを紐付け*/}
       <Box>
-        {/*タイトル*/}
-        <Box>
-          <Controller
-            // nameはInputsで定義されてるkeyに制限される
-            name="title"
-            control={control}
-            rules={validationRules.title}
-            render={({ field, fieldState }) => (
-              <TextField
-                {...field}
-                type="text"
-                label="タイトル"
-                inputProps={{ required: 'タイトルを入力' }}
-                helperText={fieldState.error?.message}
-              />
-            )}
-          />
-          {errors.title?.message && <p>{errors.title?.message}</p>}
-        </Box>
-        {/*概要*/}
-        <Box mt={2}>
-          <Controller
-            // nameはInputsで定義されてるkeyに制限される
-            name="description"
-            control={control}
-            rules={validationRules.description}
-            render={({ field, fieldState }) => (
-              <TextField
-                //{...field}
-                type="text"
-                label="概要"
-                //inputProps={{ required: 'タイトルを入力' }}
-                //error={fieldState.invalid}
-                //helperText={fieldState.error?.message}
-                //{...(fieldState.error && <p>{fieldState.error.message}</p>)}
-              />
-            )}
-          />
-        </Box>
-        {/*重要性*/}
-        <Box mt={2}>
-          <Controller
-            name="important"
-            render={({ field }) => <FormControlLabel label="重要性" {...field} control={<Checkbox />} />}
-            control={control}
-          />
-        </Box>
         {/*スコア*/}
         <Box mt={2}>
           <Controller
@@ -139,14 +68,14 @@ const reactFormNumber = () => {
                 {...field}
                 style={{ width: '150px' }}
                 type="number"
-                inputProps={{ min: 0, max: 20, step: '0.1' }}
+                inputProps={{ min: 0, max: 10, step: '0.1' }}
                 label="スコア"
                 // !!falseyな値→false
-                error={!!errors.score}
-                helperText={validationRules.score?.maxLength.message}
+                error={Boolean(errors.score)}
               />
             )}
           />
+          {errors.score?.message && <Typography variant='subtitle2' color="error">{errors.score.message}</Typography>}
         </Box>
         {/*スコア*/}
         <Box mt={2}>
@@ -171,28 +100,8 @@ const reactFormNumber = () => {
         <Grid mt={2}>
           <Grid container>
             <Grid>
-              <Button variant="contained" onClick={onCheckDirty}>
-                isDirty
-              </Button>
-            </Grid>
-            <Grid ml={2}>
-              {/*タイトルが"脆弱性"だと押下できる*/}
-              <Button disabled={watch('title') !== '脆弱性'} variant="contained" onClick={onClcikGet}>
-                watch
-              </Button>
-            </Grid>
-          </Grid>
-          <Grid mt={2} container>
-            <Grid>
-              {/*タイトルが"脆弱性"だと押下できる*/}
-              <Button variant="contained" onClick={onCheckDirty}>
-                isDirty
-              </Button>
-            </Grid>
-            <Grid ml={2}>
-              {/*タイトルが"脆弱性"だと押下できる*/}
-              <Button variant="contained" type="submit">
-                確定
+              <Button type="submit" color="secondary" variant="contained">
+                送信
               </Button>
             </Grid>
           </Grid>
