5d711fa108aef94efdde9ec83d8cc8901e173684
hoshico <melancholinista.yasu@gmail.com>
2022-11-15 22:00:35 +0900

    add yup

--- src/pages/form-yup/index.tsx
+++ src/pages/form-yup/index.tsx
@@ -1,4 +1,5 @@
 import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { Button, Container, Stack, TextField } from '@mui/material'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
@@ -11,7 +12,7 @@ type Inputs = {
 // バリデーションルール
 const schema = yup.object({
   email: yup.string().required('必須です').email('正しいメールアドレス入力してくだいさい'),
-  nama: yup.string().required('必須です'),
+  name: yup.string().required('必須です'),
   password: yup
     .string()
     .required('必須です')
@@ -20,7 +21,13 @@ const schema = yup.object({
 })
 
 const FormYup = () => {
-  const { register, handleSubmit } = useForm<Inputs>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema)
+  })
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data)
@@ -29,9 +36,22 @@ const FormYup = () => {
   return (
     <Container maxWidth="sm" sx={{ pt: 5 }}>
       <Stack spacing={3}>
-        <TextField required label="メールアドレス" type="email" {...register('email')} />
-        <TextField required label="お名前" {...register('name')} />
-        <TextField required label="パスワード" type="password" {...register('password')} />
+        <TextField
+          required
+          label="メールアドレス"
+          type="email"
+          {...register('email')}
+          error={'email' in errors}
+          helperText={errors.email?.message}
+        />
+        <TextField
+          required
+          label="お名前"
+          {...register('name')}
+          error={'name' in errors}
+          helperText={errors.name?.message}
+        />
+        <TextField required label="パスワード" type="password" {...register('password')} error={'password' in errors} helperText={errors.password?.message}/>
         <Button color="primary" variant="contained" size="large" onClick={handleSubmit(onSubmit)}>
           作成
         </Button>
