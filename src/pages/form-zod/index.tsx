5aabb61f237fd7217b7b8a79d0014fbc9d01d3d4
hoshico <melancholinista.yasu@gmail.com>
2022-11-17 08:13:41 +0900

    add schema

--- src/pages/form-zod/index.tsx
+++ src/pages/form-zod/index.tsx
@@ -1,6 +1,16 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from "zod";
+
+const Schema = z.object({
+  name: z.string()
+        .min(1, "名前を入力してください。")
+        .min(4, "4文字以上で入力してください。")
+})
+
 const FormZod = () => {
   return (
     <>
+
     </>
   )
 };
